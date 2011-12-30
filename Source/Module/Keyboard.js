66f56ddd34b2513a107eb255b34f3e5b094945c4
Noritaka Horio <holy.shared.design@gmail.com>
2011-12-30 12:15:01 +0900

    add keyboard module.

---
+++ Source/Module/Keyboard.js
@@ -0,0 +1,57 @@
+/*
+---
+name: Bootstrap.Keyboard
+
+description: 
+
+license: MIT-style
+
+authors:
+- Noritaka Horio
+
+requires:
+  - Presentation.Bootstrap/Module
+  - Presentation/Helper.Keyboard
+
+provides:
+  - Keyboard
+...
+*/
+
+(function(Module, Helper){
+
+/*
+	var bootstrap = new Moostrap('sync', Presentation.Bootstrap.Module, {
+		configurations: {
+			keyboard: {
+				//keyboard helper configuration
+			}
+		}
+	});
+	bootstrap.execute(presentation);
+*/
+Module.register('keyboard', {
+
+	title: 'setup of keyboard helper',
+
+	handler: function(presentation, configuration){
+		var bootstrapper = this,
+			helper = null;
+
+		if (!Type.isObject(configuration)){
+			configuration = {};
+		}
+
+		try {
+			helper = new Helper.Keyboard(configuration);
+			presentation.addHelper(helper);
+		} catch(exception){
+			throw exception;
+			bootstrapper.failure();
+		}
+		bootstrapper.success();
+	}
+
+});
+
+}(Presentation.Bootstrap.Module, Presentation.Helper));
---
+++ Tests/_assets/js/default.js
@@ -0,0 +1,42 @@
+(function(Presentation, Bootstrap, module){
+
+window.addEvent('load', function(){
+
+	var presentation = $('presentation');
+
+	var p = new Presentation(presentation);
+
+	var b = new Bootstrap('async', module, {
+		onBeforeBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' start');
+		},
+		onAfterBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' complete');
+		},
+		onSuccess: function(){
+			p.start();
+
+			var evt = document.createEvent('KeyEvents');
+			evt.initKeyEvent(
+				'keydown', true, true,
+				window, 
+                false, false, false, false, 
+                39, 0
+            );
+
+			var root = document.documentElement;
+			root.dispatchEvent(evt);
+
+			log( (p.getCurrentIndex() == 1) ? 'keyboard helper ok' : 'keyboard helper is disabled.' );
+
+
+		},
+		onFailure: function(){
+			log('assert ng');
+		}
+	});
+	b.execute(p);
+
+});
+
+}(Presentation, Moostrap, Presentation.Bootstrap.Module));
