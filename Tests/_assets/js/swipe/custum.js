33ea9dd5b2724907e820c22aee65e5aaa1be609d
holyshared <holy.shared.design@gmail.com>
2011-12-31 21:27:03 +0900

    add swipe test files.

---
+++ Tests/_assets/js/swipe/custum.js
@@ -0,0 +1,30 @@
+(function(Presentation, Bootstrap, module){
+
+window.addEvent('load', function(){
+
+	var presentation = $('presentation');
+
+	var p = new Presentation(presentation);
+
+	var b = new Bootstrap('async', module, {
+		configurations: {
+			swipable: true
+		},
+		onBeforeBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' start');
+		},
+		onAfterBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' complete');
+		},
+		onSuccess: function(){
+			p.start();
+		},
+		onFailure: function(){
+			log('assert ng');
+		}
+	});
+	b.execute(p);
+
+});
+
+}(Presentation, Moostrap, Presentation.Bootstrap.Module));
