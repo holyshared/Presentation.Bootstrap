c8939aca47edc5c84f2fe05434299be6b2c5ff9f
holyshared <holy.shared.design@gmail.com>
2011-12-31 21:11:42 +0900

    add controller helper test files.

---
+++ Tests/_assets/js/controller/custum.js
@@ -0,0 +1,49 @@
+(function(Presentation, Bootstrap, module){
+
+window.addEvent('load', function(){
+
+	var presentation = $('presentation');
+
+	var p = new Presentation(presentation);
+
+	var b = new Bootstrap('async', module, {
+		configurations: {
+			controller: {
+				prev: 'prevButton',
+				next: 'nextButton',
+				first: 'firstButton',
+				last: 'lastButton'
+			}
+		},
+		onBeforeBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' start');
+		},
+		onAfterBootstrap: function(key, title, current, total){
+			log(current + '/' + total + ' ' + key + ' - ' + title + ' complete');
+		},
+		onSuccess: function(){
+			p.start();
+
+			var evt = document.createEvent('MouseEvents');
+			evt.initMouseEvent(
+				'click', true, true,
+				window,
+				0, 0, 0, 0, 0, 
+                false, false, false, false, 
+                null, null
+            );
+
+			var next = presentation.getElement('.nextButton');
+			next.dispatchEvent(evt);
+
+			log( (p.getCurrentIndex() == 1) ? 'controller helper current ok' : 'controller helper current is ng.' );
+		},
+		onFailure: function(){
+			log('assert ng');
+		}
+	});
+	b.execute(p);
+
+});
+
+}(Presentation, Moostrap, Presentation.Bootstrap.Module));
